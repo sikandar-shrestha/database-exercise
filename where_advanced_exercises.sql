43ce65a1ff5d44ba9ec0cbb90e59b5b883c8378d
sikandar-shrestha <sikandarshrestha@gmail.com>
2023-10-14 17:58:24 -0500

    updated queries

--- where_advanced_exercises.sql
+++ where_advanced_exercises.sql
@@ -6,7 +6,8 @@ use employees;
 select database();
 show tables;
 
--- Q1) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. What is the employee number of the top three results?
+-- Q1) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. 
+--     What is the employee number of the top three results?
 describe employees;
 select * from employees;
 
@@ -15,18 +16,25 @@ from employees
 where first_name in ('Irena','Vidya', 'Maya') ;
 -- top three results of emp_no are 10200, 10397, and 10610
 
--- Q2) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. What is the employee number of the top three results? Does it match the previous question?
-select emp_no, first_name from employees 
-where first_name in ('Irena','Vidya') or first_name='Maya';
+-- Q2) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. 
+--     What is the employee number of the top three results? Does it match the previous question?
+select * from employees 
+where first_name ='Irena' or first_name='Vidya' or  first_name='Maya';
+
+-- A: 10200, 10397, 10610
 -- A: it's the same output as Q1
 
--- Q3) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results?
+
+
+
+-- Q3) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. 
+--     What is the employee number of the top three results?
 select emp_no, first_name
 from employees
-where first_name in ('Irena', 'Vidya', 'Maya')
+where first_name='Irena' or first_name= 'Vidya' or first_name= 'Maya'
 and gender='M';
 
--- A: 10200, 10397, 10821 emp_no
+-- A: 10200, 10397, 10610 emp_no
 
 
 
@@ -41,7 +49,8 @@ where last_name like 'E%';
 -- Q5) Find all unique last names that start or end with 'E'.
 select distinct last_name 
 from employees
-where last_name like 'E%E';
+where last_name like 'E%'
+or last_name like '%E';
 
 
 -- Q6) Find all unique last names that end with E, but does not start with E?
@@ -59,16 +68,22 @@ where last_name like 'E%E';
 
 -- Q8) Find all current or previous employees hired in the 90s. Enter a comment with the top three employee numbers.
 
-select emp_no, hire_date
+select *
 from employees
 where hire_date like '199%';
+-- where hire_date BETWEEN 19900101 AND 19991225
+-- WHERE hire_date BKETWEEN '1990-01-01' AND '1999-12-31'
+-- WHERE year(hir_date) BETWEEN 1990 AND 1999
+
+
+
 
 -- A: 10008, 10011, 10012
 
 
 -- Q9) Find all current or previous employees born on Christmas. Enter a comment with the top three employee numbers.
 
-select emp_no, birth_date
+select *
 from employees
 where birth_date like '%-12-25'
 ;
@@ -77,7 +92,7 @@ where birth_date like '%-12-25'
 
 -- Q10) Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the top three employee numbers.
 
-select emp_no, birth_date, hire_date
+select *
 from employees
 where hire_date like '199%' and birth_date like '%-12-25';
 
